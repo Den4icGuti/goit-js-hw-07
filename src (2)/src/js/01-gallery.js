b750b6cc807e78ff40be2f67e4a6302645e3a1ca
Den4icGuti <goos7412@gmail.com>
2021-12-15 16:15:57 +0200

    Сделано второе дз

--- src (2)/src/js/01-gallery.js
+++ src (2)/src/js/01-gallery.js
@@ -3,23 +3,48 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryRef = document.querySelector('.gallery');
 
-galleryRef.insertAdjacentHTML('beforebegin',createImg)
+const galleryMarcup = createGalleryMarcup();
 
+galleryRef.insertAdjacentHTML('beforeend', galleryMarcup);
 
-const createImg = galleryItems.map(({ preview, original, description }) => { 
+galleryRef.addEventListener('click', createOpenModaImg);
+
+galleryRef.addEventListener('keydown', onCloseModal);
+
+function createGalleryMarcup() { 
   
-  return basicLinghtbox.create(`
-  <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>
-  `)
+  return  galleryItems.map(({ preview, original, description }) => 
+   `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+    `
+    ).join('')
+};
 
-}).join('')
+let img = ""
 
+function createOpenModaImg(evt) { 
+  evt.preventDefault();
+  img = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  
+  img.show()
+}
+
+
+function onCloseModal(evt) { 
+  if (evt.code === "Escape") { 
+    img.close()
+  }
+};
+
+
+ 
--- src (2)/src/js/02-lightbox.js
+++ src (2)/src/js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const marCupGallery = galleryMarcup();
+
+galleryRef.insertAdjacentHTML('beforeend', marCupGallery);
+
+
+
+
+function galleryMarcup() { 
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+    `
+   }).join('')
+};
+
+
+
+let img = ''
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+gallery.on('show.simplelightbox', function (e) {
+	img = `<img = "${e.target.dataset.source}">`
+});
+
+
